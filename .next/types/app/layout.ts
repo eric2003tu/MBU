9871e8b9621ba5c3af4f97ff05ff58d3f47ea4bf
RUYANGA <ruyangam15@gmail.com>
2026-02-26 20:44:44 +0200

    update footer and readme

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\mukar\Desktop\Other\property-hub\app\layout.tsx
+// File: /home/merci-ruyanga/Desktop/RUYANGA/Real Estate/MBU/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\mukar\Desktop\Other\property-hub\app\page.tsx
+// File: /home/merci-ruyanga/Desktop/RUYANGA/Real Estate/MBU/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
